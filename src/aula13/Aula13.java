24fa7cb3a44e136345befcd88763780252d8cd46
Davi Augusto <davi.cianorte@gmail.com>
2024-01-01 20:33:20 -0300

    adicionando projeto

---
+++ src/aula13/Aula13.java
@@ -0,0 +1,27 @@
+package aula13;
+
+public class Aula13 {
+    public static void main(String[] args) {
+        // Programa Principal
+        Cachorro c = new Cachorro();
+
+        // reação de frase
+        //c.reagir("Olá");
+        //c.reagir("Vai apanhar");
+
+
+        // reação de horas
+        //c.reagir(11,45);
+        //c.reagir(19,00);
+
+        // reação de dono
+        //c.reagir(true);
+        //c.reagir(false);
+
+        // reação idade, peso
+        c.reagir(2, 12.5f);
+        c.reagir(17, 4.5f);
+
+    }
+    
+}
